eda64e494130317dcd192eaca5418b13029af86b
Light Guardhyan <35728599+aircleanapp@users.noreply.github.com>
2019-11-05 21:45:22 +0200

    remove maps and forms

--- js/main.js
+++ js/main.js
@@ -193,19 +193,19 @@ jQuery(function($) {'use strict';
 	});
 
 	// Contact form
-	var form = $('#main-contact-form');
-	form.submit(function(event){
-		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		$.ajax({
-			url: $(this).attr('action'),
-			beforeSend: function(){
-				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> sending email...</p>').fadeIn() );
-			}
-		}).done(function(data){
-			form_status.html('<p class="text-success">Thank you for contacting us! You will get a reply within 3 business days.</p>').delay(3000).fadeOut();
-		});
-	});
+//	var form = $('#main-contact-form');
+//	form.submit(function(event){
+//		event.preventDefault();
+//		var form_status = $('<div class="form_status"></div>');
+//		$.ajax({
+//			url: $(this).attr('action'),
+//			beforeSend: function(){
+//				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> sending email...</p>').fadeIn() );
+//			}
+//		}).done(function(data){
+//			form_status.html('<p class="text-success">Thank you for contacting us! You will get a reply within 3 business days.</p>').delay(3000).fadeOut();
+//		});
+//	});
 
 	//Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
@@ -213,21 +213,21 @@ jQuery(function($) {'use strict';
 	});
 
 	//Google Map
-	var latitude = $('#google-map').data('latitude');
-	var longitude = $('#google-map').data('longitude');
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
-		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-	}
-	google.maps.event.addDomListener(window, 'load', initialize_map);
+//	var latitude = $('#google-map').data('latitude');
+//	var longitude = $('#google-map').data('longitude');
+//	function initialize_map() {
+//		var myLatlng = new google.maps.LatLng(latitude,longitude);
+//		var mapOptions = {
+//			zoom: 14,
+//			scrollwheel: false,
+//			center: myLatlng
+//		};
+//		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+//		var marker = new google.maps.Marker({
+//			position: myLatlng,
+//			map: map
+//		});
+//	}
+//	google.maps.event.addDomListener(window, 'load', initialize_map);
 
 });
